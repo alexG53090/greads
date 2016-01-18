170977948fdb2e2652d05ab81270f79185a68298
Alex Bennett <alexandergraybennett@gmail.com>
2016-01-18 16:46:48 -0700

    dash of knex

---
+++ knexfile.js
@@ -0,0 +1,10 @@
+module.exports = {
+  development: {
+    client: 'pg',
+    connection: 'postgres://wptugffjhbpqta:V8lvqBLY6Ry12tRP0GW7mQ87Zj@ec2-54-83-40-119.compute-1.amazonaws.com:5432/d6b88ef20b57to?ssl=true'
+  },
+  // production: {
+  //   client: 'pg',
+  //   connection: 'postgres://wptugffjhbpqta:V8lvqBLY6Ry12tRP0GW7mQ87Zj@ec2-54-83-40-119.compute-1.amazonaws.com:5432/d6b88ef20b57to?ssl=true'
+  // }
+};
